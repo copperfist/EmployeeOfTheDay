e8e6d205097b505174c62d7510a7c9f7902c607b
DanielHarrisStudent <56642329+DanielHarrisStudent@users.noreply.github.com>
2019-10-21 15:48:40 +0100

    Trying to code till and exit AI

--- Assets/Scripts/NpcBehaviour.cs
+++ Assets/Scripts/NpcBehaviour.cs
@@ -6,20 +6,45 @@ using UnityEngine.AI;
 public class NpcBehaviour : MonoBehaviour
 {
     public NavMeshAgent agent;
-    public GameObject[] targetPoints;
-    private GameObject currentPoint;
-    private int index;
+    public GameObject[] targetWaypoints;
+    public GameObject tillWaypoint;
+    public GameObject exitWaypoint;
 
-    private void Start()
+
+    private GameObject currentWaypoint;
+    private int index;
+    private float minDistance = 0.3f;
+    private float distance;
+
+    private void Start() //Pick a random waypoint to start
     {
-        targetPoints = GameObject.FindGameObjectsWithTag("targets");
-        index = Random.Range(0, targetPoints.Length);
-        currentPoint = targetPoints[index];
+        targetWaypoints = GameObject.FindGameObjectsWithTag("targets");
+        tillWaypoint = GameObject.FindGameObjectWithTag("tillPayment");
+        index = Random.Range(0, targetWaypoints.Length);
+        currentWaypoint = targetWaypoints[index];
+        Debug.Log("Going to" + currentWaypoint);
     }
 
 
     private void Update()
     {
-        agent.SetDestination(currentPoint.transform.position);
+        distance = Vector3.Distance(transform.position, currentWaypoint.transform.position);
+        Debug.Log(distance);
+        CheckDistanceToWaypoint(distance);
+        agent.SetDestination(currentWaypoint.transform.position);
+
+    }
+
+    void CheckDistanceToWaypoint(float currentDistance)
+    {
+        if(currentDistance <= minDistance)
+        {
+            Debug.Log("To the till");
+            SendToTill();
+        }
+    }
+    void SendToTill()
+    {
+        currentWaypoint.transform.position = tillWaypoint.transform.position;
     }
 }

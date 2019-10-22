a807f9c18b04bc41e420ea291fb4026efa9b6351
DanielHarrisStudent <56642329+DanielHarrisStudent@users.noreply.github.com>
2019-10-22 12:54:27 +0100

    trying to get files up to date

---
+++ Assets/Scripts/NpcBehaviour.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NpcBehaviour : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public GameObject[] targetPoints;
+    private GameObject currentPoint;
+    private int index;
+
+    private void Start()
+    {
+        targetPoints = GameObject.FindGameObjectsWithTag("targets");
+        index = Random.Range(0, targetPoints.Length);
+        currentPoint = targetPoints[index];
+    }
+
+
+    private void Update()
+    {
+        agent.SetDestination(currentPoint.transform.position);
+    }
+}

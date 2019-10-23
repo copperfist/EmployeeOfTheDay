9c52fca49b5ae795d70da919d7741a9c9012c9b3
DanielHarrisStudent <56642329+DanielHarrisStudent@users.noreply.github.com>
2019-10-23 16:09:25 +0100

    Added player delete Script

---
+++ Assets/Scripts/NpcDelete.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NpcDelete : MonoBehaviour
+{
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("NPC"))
+        {
+            Destroy(other.gameObject);
+        }
+    }
+}

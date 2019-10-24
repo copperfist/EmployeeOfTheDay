f6beb6151d0bb7bd5ac24e549f981df646dcd1b9
Ellie Harrodine <ellieharrodine@gmail.com>
2019-10-24 14:03:27 +0100

    timer added
    timer and UI added - game over screen

---
+++ Assets/Scripts/TimerGameOver.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class TimerGameOver : MonoBehaviour
+{
+    int countDownStartValue = 10;
+    public Text timerUI;
+    void Start()
+    {
+        countDownTimer();
+    }
+    
+    void countDownTimer()
+    {
+        if (countDownStartValue > 0)
+        {
+            TimeSpan spanTime = TimeSpan.FromSeconds(countDownStartValue); 
+            timerUI.text = spanTime.Minutes + ":" + spanTime.Seconds;
+            countDownStartValue--;
+            Invoke("countDownTimer", 1.0f); 
+        }else
+        {
+            FindObjectOfType<GameManager>().EndGame();
+            timerUI.text = "Game Over";
+            
+        }
+
+    }
+
+
+}
